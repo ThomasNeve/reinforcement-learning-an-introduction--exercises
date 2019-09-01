24d1f64b2f8c7cd2e8f8ed83e2b81f895a17289f
ThomasNe <thomas.neve0@gmail.com>
2019-09-01 18:10:49 +0200

    Update cartPole_v1.py

--- cartPole_balancing/cartPole_v1.py
+++ cartPole_balancing/cartPole_v1.py
@@ -1,34 +1,86 @@
 import gym
 import numpy as np
 
+#github source: https://github.com/openai/gym/blob/master/gym/envs/classic_control/cartpole.py
+
 env = gym.make('CartPole-v0')
-env.reset()
+
+EPISODES = 1
+RENDER_INTERVAL = 50
+PLOT_INTERVAL = 10
 
 #observation = [cart_position=[-4.8,4.8], cart_velocity=[inf, inf], pole angle=[-24,24]deg, pole velocity at tip=[-Inf,Inf]]
-
 BUCKET_SIZE = [20, 1, 20, 1] #ignore velocity
+space_high = env.observation_space.high[BUCKET_SIZE==1] = 1
+space_low = env.observation_space.low[BUCKET_SIZE==1] = 1
+WINDOW_SIZE = (space_high-space_low)/BUCKET_SIZE
+
 ACTION_SPACE = env.action_space.n
 
 Q_values = np.ones(BUCKET_SIZE + [ACTION_SPACE])
 
-done = False
-
 LR = 0.5
 DISCOUNT = 0.9
 
-def state_index(state_observation):
+#goal is specific for every environment
+SUCCES_COND = [(-2.4, 2.4), (-np.inf, np.inf), (-12, 12), (-np.inf, np.inf)]
 
-    index = (state_observation - env.observation_space.low)/BUCKET_SIZE
-    return index.astype(int)
 
-while not done:
+def checkGoal(state):
+    
+    goal = True
 
-    obesrvation, reward, done, _ = env.step(env.action_space.sample())
-    env.render()
+    for i in range(len(state)):
 
-    print()
-    print(obesrvation)
-    print(reward)
+        if SUCCES_COND[i][0]>state[i] or SUCCES_COND[i][1]<state[i]:
+            goal = False
+
+    return goal
+
+
+def stateIndex(state_observation):
+
+    index = (state_observation - env.observation_space.low)/WINDOW_SIZE
+    index[BUCKET_SIZE == 1] = 1 #environment variable is not discretized
+
+    return tuple(index.astype(int))
+
+episode_rewards = np.array([])
+
+for episode in range(EPISODES):
+
+    observation = env.reset()
+    discrete_state = stateIndex(observation)
+
+    episode_reward = 0
+
+    done = False
+
+    while not done:
+
+        action = np.argmax(Q_values[discrete_state])
+        observation, reward, done, _ = env.step(action)
+
+        episode_reward += reward
+
+        if episode%RENDER_INTERVAL == 0:
+            env.render()
+
+        if not done:
+
+            new_discrete_state = stateIndex(observation)
+
+            Q_old = Q_values[discrete_state + (action,)]
+            Q_max_future = Q_values[new_discrete_state]
+
+            Q_new = (1-LR)*Q_old + LR*(reward + DISCOUNT*Q_max_future)
+
+            Q_values[discrete_state + (action,)] = Q_new
+            
+        elif checkGoal():
+            pass
+
+        discrete_state = new_discrete_state
 
 
 env.close()
